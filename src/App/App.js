b55e62c1c8c97fb31483d3ddf98d578b5443262d
sravan <vsravan707@gmail.com>
2018-11-30 16:50:43 +0100

    wip change

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react'
 import './App.css'
-import { CreateMarkup } from '../components'
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <CreateMarkup html='<h1>accomplice-2019</h1>' />
-        </header>
+      <div className='App-container'>
+        <nav className='nav-bar'>
+          <ul>
+            <li>Services</li>
+            <li>Insights</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+        <section>
+          <p>
+            We team up to create solutions that make organizations, society,
+            services and products better.
+          </p>
+        </section>
       </div>
     )
   }
